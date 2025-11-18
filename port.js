894b3b52db01db046cfa8a35818649f154b90474
azmccormicks-beep <azmccormicks@gmail.com>
2025-11-18 13:33:47 -0700

    js

---
+++ port.js
@@ -0,0 +1,31 @@
+//template_gshcxuu
+//service_3wh8sjb
+//user_izP1FCYbL8nq5mcD9
+
+function contact(event) {
+  event.preventDefault(); 
+  const loading = document.querySelector('.modal__overlay--loading');
+  const success = document.querySelector('.modal__overlay--success');  
+  loading.classList += ' modal__overlay--visible';
+  emailjs
+  .sendForm(
+    'service_3wh8sjb',
+    'template_gshcxuu',
+     event.target,
+    'izP1FCYbL8nq5mcD9'
+  )
+  .then(() => {
+    loading.classList.remove('modal__overlay--visible');
+    success.classList += ' modal__overlay--visible';
+    }).catch(() => {
+        loading.classList.remove('modal__overlay--visible');
+        alert("The email service is temporarily unavailable. Please contact me directly on azmccormicks@gmail.com"
+
+        );
+    })
+
+}
+
+
+    
+  
